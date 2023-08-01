78eb286bab3e4dbab9796d4ffc14d1e186bb8d17
Abdul Aziz <azizadnan370@gmail.com>
2023-08-01 15:17:20 +0530

    4th day

--- app.js
+++ app.js
@@ -1,51 +1,155 @@
-// app.js
 const express = require("express");
-const { validateCreateUser } = require("./validatorMiddleware");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// Initialize Express app
 const app = express();
-
 app.use(express.json());
 
-let users = [];
-
-// Route to create a new user
-app.post("/users", validateCreateUser, (req, res) => {
-  const newUser = req.body;
-  users.push(newUser);
-  res.json(newUser);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-// Route to read all users
-app.get("/users", (req, res) => {
-  res.json(users);
+// Define a user schema and model
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, default: "user" },
 });
 
-// Route to update a user
-app.put("/users/:id", (req, res) => {
-  const userId = parseInt(req.params.id);
-  const updatedUser = req.body;
+const User = mongoose.model("User", userSchema);
 
-  // Find the user in the array based on the provided ID
-  const userIndex = users.findIndex((user) => user.id === userId);
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+  const token = req.header("Authorization");
 
-  if (userIndex !== -1) {
-    // Update the user object with the new data
-    users[userIndex] = { ...users[userIndex], ...updatedUser };
-    res.json(users[userIndex]);
-  } else {
-    res.status(404).json({ message: "User not found" });
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token missing" });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid or expired token" });
+    }
+
+    req.user = decodedToken;
+    next();
+  });
+};
+
+// Login API
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
-// Route to delete a user
-app.delete("/users/:id", (req, res) => {
-  const userId = parseInt(req.params.id);
-  users = users.filter((user) => user.id !== userId);
-  res.json({ message: "User deleted successfully" });
+// CRUD APIs for user management
+// Note: These APIs require JWT token verification middleware (verifyToken)
+
+// Get all users
+app.get("/api/users", verifyToken, async (req, res) => {
+  try {
+    const users = await User.find({}, { password: 0 });
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Get single user
+app.get("/api/users/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id, { password: 0 });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Update user
+app.put("/api/users/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+  const { username, password, role } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { username, password, role },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Delete user
+app.delete("/api/users/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
