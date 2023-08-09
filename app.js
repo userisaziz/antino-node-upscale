f24fe59de2d1463adb3a4dfdb80bde5146e6e516
Abdul Aziz <azizadnan370@gmail.com>
2023-08-09 18:15:04 +0530

    9th

--- app.js
+++ app.js
@@ -1,155 +1,29 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const multer = require("multer");
 
-// Initialize Express app
 const app = express();
-app.use(express.json());
+const port = 3000;
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+// Configure Multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-// Define a user schema and model
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, default: "user" },
-});
+const upload = multer({ storage: storage });
 
-const User = mongoose.model("User", userSchema);
-
-// Middleware to verify JWT token
-const verifyToken = (req, res, next) => {
-  const token = req.header("Authorization");
-
-  if (!token) {
-    return res.status(401).json({ message: "Authorization token missing" });
+// Define the upload route
+app.post("/upload", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded." });
   }
-
-  jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid or expired token" });
-    }
-
-    req.user = decodedToken;
-    next();
-  });
-};
-
-// Login API
-app.post("/api/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
-    const token = jwt.sign(
-      { id: user._id, role: user.role },
-      process.env.SECRET_KEY,
-      {
-        expiresIn: "1h",
-      }
-    );
-
-    res.json({ token });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
+  res.json({ message: "File uploaded successfully." });
 });
 
-// CRUD APIs for user management
-// Note: These APIs require JWT token verification middleware (verifyToken)
-
-// Get all users
-app.get("/api/users", verifyToken, async (req, res) => {
-  try {
-    const users = await User.find({}, { password: 0 });
-    res.json(users);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-// Get single user
-app.get("/api/users/:id", verifyToken, async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const user = await User.findById(id, { password: 0 });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-// Update user
-app.put("/api/users/:id", verifyToken, async (req, res) => {
-  const { id } = req.params;
-  const { username, password, role } = req.body;
-
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      { username, password, role },
-      { new: true }
-    );
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json(updatedUser);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-// Delete user
-app.delete("/api/users/:id", verifyToken, async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const deletedUser = await User.findByIdAndDelete(id);
-
-    if (!deletedUser) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json({ message: "User deleted successfully" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-// Start the server
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
